ffad140cd2b72896302aba02c85d8be435837f22
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-05-10 21:27:07 -0500

    adding morgan middleware

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+//adding morgan middleware
+app.use(morgan('dev'));
+
+//set up express to serve files from the public folder
+app.use(express.static(__dirname + '/public'));
 
 //set up server with a use method and a callback
 app.use((req, res,) => {
-    console.log(req.header);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
